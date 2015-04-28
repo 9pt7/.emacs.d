9736048216c5d613bb8aa76c22b49c717bb792df
Peter Thompson <peter@peter.deb>
2015-04-27 23:43:41 -0300

    Remove pymacs

--- init.el
+++ init.el
@@ -539,14 +539,6 @@ The app is chosen from your OS's preference."
 ;; (add-to-list 'load-path "~/.emacs.d/themes")
 ;; (add-to-list 'load-path "~/.emacs.d/pymacs")
 
-;; (require 'pymacs)
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-;; (autoload 'pymacs-autoload "pymacs")
-;; (pymacs-load "ropemacs" "rope-")
 
 ;; Finally start emacs server
 (server-start)
