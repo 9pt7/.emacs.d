49eab032f719077542f744e8c3be0b26af2dc3cc
Peter Thompson <peter@peter.deb>
2015-04-28 00:48:04 -0300

    Dont track elpa files

--- elpa/rw-hunspell-0.2/rw-hunspell-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; rw-hunspell-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("rw-hunspell.el") (21822 60571 595583
-;;;;;;  396000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rw-hunspell-autoloads.el ends here
