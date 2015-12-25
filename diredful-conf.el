50c58f33312e3edf3ece9bf1f4561fc4654c8a40
peter <peter@localhost.localdomain>
2015-12-24 22:08:28 -0500

    Added sh color.

--- diredful-conf.el
+++ diredful-conf.el
@@ -1,4 +1,4 @@
 
 ("Text" "Media" "" "PDF")
 
-(("Text" (:foreground "red") "m py c el cc cpp tex txt org bib" nil nil nil nil) ("Media" (:foreground "dark violet") "jpeg jpg svg png gif mp4 mp3 flac wav opus" nil nil nil nil) ("" (:weight bold :foreground "dark orange") "tex org txt" nil nil nil nil) ("" nil "" nil nil nil nil) ("PDF" (:foreground "dark green") "pdf epub dvi" nil nil nil nil))
+(("Text" (:foreground "red") "m py c el cc cpp tex txt org bib sh" nil nil nil nil) ("Media" (:foreground "dark violet") "jpeg jpg svg png gif mp4 mp3 flac wav opus" nil nil nil nil) ("" (:weight bold :foreground "dark orange") "tex org txt" nil nil nil nil) ("" nil "" nil nil nil nil) ("PDF" (:foreground "dark green") "pdf epub dvi" nil nil nil nil))
