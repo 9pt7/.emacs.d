be34739fa37f4c12daf49ead588cb59590d07598
peter <peter@Dell-PC>
2017-02-27 15:56:48 -0500

    Fixed c++ template.

--- cpp-template/main.cc
+++ cpp-template/main.cc
@@ -1,18 +1,18 @@
 /*! A test class */
 class Afterdoc_Test
 {
-  public:
+public:
     /** An enum type.
      *  The documentation block cannot be put after the enum!
      */
     enum EnumType
     {
-      int EVal1,     /**< enum value 1 */
-      int EVal2      /**< enum value 2 */
+        EVal1,     /**< enum value 1 */
+        EVal2      /**< enum value 2 */
     };
     void member();   //!< a member function.
 
-  protected:
+protected:
     int value;       /*!< an integer value */
 };
 
